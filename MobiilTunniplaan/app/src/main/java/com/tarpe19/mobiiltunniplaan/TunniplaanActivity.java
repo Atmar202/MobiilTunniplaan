1e56568c12cb6c7c0f3277ea8941d2844fef1e74
Atmar202 <71074700+Atmar202@users.noreply.github.com>
2021-06-10 09:36:57 +0300

    Ebavajalike massiivide kustutamine

--- MobiilTunniplaan/app/src/main/java/com/tarpe19/mobiiltunniplaan/TunniplaanActivity.java
+++ MobiilTunniplaan/app/src/main/java/com/tarpe19/mobiiltunniplaan/TunniplaanActivity.java
@@ -17,17 +17,11 @@ public class TunniplaanActivity extends AppCompatActivity implements AdapterView
 
     TextView aineInfo, klassiInfo, aineInfo2, klassiInfo2, mealbreak, groupHeader;
     String selectionText;
-    int index = 4;
-    // Cannot be used in for loop
-    String[] mTime = new String[]{"11:50 - 12:35", "12:40 - 15:55", "8:30 - 11:45", "11:00 - 11:45", "12:40 - 14:15", "8:30 - 11:00", "10:10 - 13:25", "13:30 - 15:05"};
-    String[] mSubject = new String[]{"Agiilsed tarkvaraarenduse metoodikad", "Andmebaasisüsteemide alused", "Vene keel", "Projektijuhtimine"};
-    String[] mTeachers = new String[]{"Aleksandra Psenitsner","Ingvar Deresivski", "Irina Merkulova"};
-    String[] mClass = new String[]{"B219","A116", "E10"};
-    // Used in for loop for case 0 and 2
-    String[] time = new String[]{"8:30 - 10:05", "10:10 - 11:45", "12:40 - 14:15"};
-    String[] subject = new String[]{"Inglise keel (agiilsed tarkvararaarenduse metoodikad)", "Ajalugu ja ühiskonnaõpetus", "Võrgurakendused"};
-    String[] sClass = new String[]{"B228", "B238", "A236"};
-    String[] teachers = new String[]{"Margit Uiboaid", "Silva Kiveste", "Kristjan Kivikangur"};
+    int index = 7;
+    String[] time = new String[]{"8:30 - 10:55", "11:00 - 11:45", "12:40 - 14:15", "11:50 - 12:35", "12:40 - 15:55", "8:30 - 11:45", "11:00 - 11:45", "12:40 - 14:15", "8:30 - 11:00", "10:10 - 13:25", "13:30 - 15:05", "14:20 - 15:55", "8:30 - 10:05", "10:10 - 11:45"};
+    String[] subject = new String[]{"Inglise keel (agiilsed tarkvararaarenduse metoodikad)", "Ajalugu ja ühiskonnaõpetus", "Võrgurakendused", "Agiilsed tarkvaraarenduse metoodikad", "Andmebaasisüsteemide alused", "Vene keel", "Projektijuhtimine"};
+    String[] sClass = new String[]{"B228", "B238", "A236", "B219","A116", "E10"};
+    String[] teachers = new String[]{"Margit Uiboaid", "Silva Kiveste", "Kristjan Kivikangur", "Aleksandra Psenitsner","Ingvar Deresivski", "Irina Merkulova"};
     Spinner spinner1, spinner2;
 
     @Override
@@ -71,69 +65,69 @@ public class TunniplaanActivity extends AppCompatActivity implements AdapterView
                     switch (position) {
                         // ained lisada läbi for tsükli
                         case 0:
-                            for (int i = 0; i < time.length; i++) {
+                            for (int i = 0; i < 2; i++) {
                                 if (subject[i].length() > 25 && subject[i].substring(25).length() > 10) {
                                     aineInfo.append(time[i] + " " + subject[i].substring(0, 13) + "\n" + subject[i].substring(13, 30) + "\n" + subject[i].substring(30));
                                     klassiInfo.append(sClass[i] + " / " + teachers[i]);
                                     klassiInfo.append("\n\n");
-                                    if (i != time.length - 1) {
+                                    if (i != 1) {
                                         klassiInfo.append("\n");
                                         aineInfo.append("\n");
                                     }
                                 } else if (subject[i].length() > 10) {
                                     aineInfo.append(time[i] + " " + subject[i].substring(0, 8) + "\n" + subject[i].substring(8));
                                     klassiInfo.append(sClass[i] + " / " + teachers[i] + "\n");
-                                    if (i != time.length - 1) {
+                                    if (i != 1) {
                                         klassiInfo.append("\n");
                                         aineInfo.append("\n");
                                     }
                                 } else {
                                     aineInfo.append(time[i] + " " + subject[i]);
                                     klassiInfo.append(sClass[i] + " / " + teachers[i]);
-                                    if (i != time.length - 1) {
+                                    if (i != 1) {
                                         klassiInfo.append("\n");
                                         aineInfo.append("\n");
                                     }
                                 }
                             }
-                            mealbreak.setText(mTime[0] + " Söögivahetund");
-                            aineInfo2.setText(mTime[1] + " " + subject[2].substring(0, 8) + "\n" + subject[2].substring(8));
+                            mealbreak.setText(time[3] + " Söögivahetund");
+                            aineInfo2.setText(time[4] + " " + subject[2].substring(0, 8) + "\n" + subject[2].substring(8));
                             klassiInfo2.setText(sClass[2] + " / " + teachers[2]);
                             break;
                         case 1:
-                            aineInfo.setText(mTime[2] + " " + mSubject[0].substring(0, 13) + "\n" + mSubject[0].substring(13));
+                            aineInfo.setText(time[5] + " " + subject[3].substring(0, 13) + "\n" + subject[3].substring(13));
                             klassiInfo.setText(sClass[2] + " / " + teachers[2]);
-                            mealbreak.setText(mTime[0] + " Söögivahetund");
+                            mealbreak.setText(time[3] + " Söögivahetund");
                             for (int i = 2; i < 3; i++) {
-                                aineInfo2.append(mTime[1] + " " + subject[i].substring(0, 8) + "\n" + subject[i].substring(8));
+                                aineInfo2.append(time[4] + " " + subject[i].substring(0, 8) + "\n" + subject[i].substring(8));
                                 klassiInfo2.append(sClass[i] + " / " + teachers[i]);
                             }
                             break;
                         case 2:
-                            aineInfo.setText(mTime[5] + " " + mSubject[1].substring(0, 8) + "\n" + mSubject[1].substring(8));
-                            klassiInfo.setText(mClass[1] + " / " + mTeachers[1]);
-                            mealbreak.setText(mTime[3] + " Söögivahetund");
-                            for (int i = 0; i < time.length; i++) {
+                            aineInfo.setText(time[0] + " " + subject[4].substring(0, 8) + "\n" + subject[4].substring(8));
+                            klassiInfo.setText(sClass[4] + " / " + teachers[4]);
+                            mealbreak.setText(time[6] + " Söögivahetund");
+                            for (int i = 0; i < 3; i++) {
                                 if (subject[i].length() > 25 && subject[i].substring(25).length() > 10) {
-                                    aineInfo2.append(mTime[0] + " " + subject[i].substring(0, 13) + "\n" + subject[i].substring(13, 30) + "\n" + subject[i].substring(30));
+                                    aineInfo2.append(time[3] + " " + subject[i].substring(0, 13) + "\n" + subject[i].substring(13, 30) + "\n" + subject[i].substring(30));
                                     klassiInfo2.append(sClass[i] + " / " + teachers[i]);
                                     klassiInfo2.append("\n\n");
-                                    if (i != time.length - 1) {
+                                    if (i != 2) {
                                         klassiInfo2.append("\n");
                                         aineInfo2.append("\n");
                                     }
                                 } else if (subject[i].length() > 10) {
-                                    if (i > 1) index = 1;
-                                    aineInfo2.append(mTime[index] + " " + subject[i].substring(0, 8) + "\n" + subject[i].substring(8));
+                                    if (i > 1) index = 11;
+                                    aineInfo2.append(time[index] + " " + subject[i].substring(0, 8) + "\n" + subject[i].substring(8));
                                     klassiInfo2.append(sClass[i] + " / " + teachers[i] + "\n");
-                                    if (i != time.length - 1) {
+                                    if (i != 2) {
                                         klassiInfo2.append("\n");
                                         aineInfo2.append("\n");
                                     }
                                 } else {
                                     aineInfo2.append(time[i] + " " + subject[i]);
                                     klassiInfo2.append(sClass[i] + " / " + teachers[i]);
-                                    if (i != time.length - 1) {
+                                    if (i != 2) {
                                         klassiInfo2.append("\n");
                                         aineInfo2.append("\n");
                                     }
@@ -141,34 +135,25 @@ public class TunniplaanActivity extends AppCompatActivity implements AdapterView
                             }
                             break;
                         case 3:
-                            aineInfo.setText(time[2] + " " + mSubject[0].substring(0, 13) + "\n" + mSubject[0].substring(13));
+                            aineInfo.setText(time[5] + " " + subject[3].substring(0, 13) + "\n" + subject[4].substring(13));
                             klassiInfo.setText(sClass[2] + " / " + teachers[2]);
                             break;
                         case 4:
-                            for (int i = 0; i < 2; i++) {
-                                klassiInfo.append(mClass[i] + " / " + mTeachers[i]);
-                                if (i < 1) {
-                                    aineInfo.append(time[i] + " " + mSubject[2]);
-                                    klassiInfo.append("\n");
-                                    aineInfo.append("\n");
-                                } else {
-                                    aineInfo.append(time[i] + " " + mSubject[1].substring(0, 8) + "\n" + mSubject[1].substring(8));
-                                }
-                            }
-                            //aineInfo.setText("8:30 - 10:05 Vene keel\n10:10 - 11:45 Andmebaasi\nsüsteemide alused");
-                            //klassiInfo.setText("B219 / Aleksandra\nPsenitsner\nA116 / Ingvar Deresivski");
-
-                            mealbreak.setText(mTime[0] + " Söögivahetund");
-                            aineInfo2.setText(time[2] + " " + mSubject[1].substring(0, 8) + "\n" + mSubject[1].substring(8));
-                            klassiInfo2.setText(mClass[1] + " / " + mTeachers[1]);
+                            klassiInfo.append(sClass[3] + " / " + teachers[3]+"\n");
+                            aineInfo.append(time[12] + " " + subject[5]+"\n");
+                            klassiInfo.append(sClass[4] + " / " + teachers[4]+"\n");
+                            aineInfo.append(time[13] + " " + subject[4].substring(0, 8) + "\n" + subject[4].substring(8));
+                            mealbreak.setText(time[3] + " Söögivahetund");
+                            aineInfo2.setText(time[7] + " " + subject[4].substring(0, 8) + "\n" + subject[4].substring(8));
+                            klassiInfo2.setText(sClass[4] + " / " + teachers[4]);
                             break;
                     }
                 } else if(selectionText.equals("TARge19")) {
                     if(position == 2){
-                        aineInfo.append(mTime[6] + " " + mSubject[0].substring(0, 13) + "\n" + mSubject[0].substring(13)+"\n");
+                        aineInfo.append(time[9] + " " + subject[3].substring(0, 13) + "\n" + subject[3].substring(13)+"\n");
                         klassiInfo.append(sClass[2] + " / " + teachers[2]+"\n\n");
-                        aineInfo.append(mTime[7] + " " + mSubject[3] + "\n");
-                        klassiInfo.append(mClass[2] + " / " + mTeachers[2]);
+                        aineInfo.append(time[10] + " " + subject[6] + "\n");
+                        klassiInfo.append(sClass[5] + " / " + teachers[5]);
                     }
                 }
             }
